694609ed85d00a70d1d77c737dd12481a26ea923
gui-soares <eng.guilhermemsoares@gmail.com>
2019-07-24 16:44:05 -0300

    Estilizando lista de repos

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import PropTypes from 'prop-types';
 
-import { View, Text, StatusBar, ActivityIndicator } from 'react-native';
+import {
+  View,
+  StatusBar,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 import api from '~/services/api';
 
 import Header from '~/components/Header';
 
+import RepositoryItem from './RepositoryItem';
 import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
@@ -33,14 +40,23 @@ export default class Repositories extends Component {
     this.setState({ data, loading: false });
   }
 
-  renderList = () => (
-    <Text>Lista</Text>
-  )
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />
+
+  renderList = () => {
+    const { data } = this.state;
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+      />
+    );
+  };
 
   render() {
     const { loading } = this.state;
     return (
-      <View>
+      <View style={styles.container}>
         <StatusBar backgroundColor="#fff" barStyle="dark-content" />
         <Header title="Repositórios" />
         { loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
